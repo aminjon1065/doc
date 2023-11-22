31858a5c62d614126ee2bbaddd2b9eb34ea65b5c
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2023-11-22 14:30:02 +0500

    0.0.1

--- resources/views/emails/documents/created.blade.php
+++ resources/views/emails/documents/created.blade.php
@@ -1,10 +1,9 @@
 <x-mail::message>
     # Здравствуйте!
-    Вам отправлен новый документ: {{ $document->title }}
-<x-mail::button :url="url('/documents/'.$document->id)">
-Button Text
-</x-mail::button>
-
-Спасибо,<br>
-{{ config('app.name') }}
+    Напоминание, по поводу документа: {{ $document->title }}
+    <x-mail::button :url="url('/documents/'.$document->id)">
+        Открыть документ
+    </x-mail::button>
+    Спасибо,<br>
+    {{ config('app.name') }}
 </x-mail::message>
