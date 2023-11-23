9210f0d423e36660477a88ee412551fed205f5e2
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2023-11-23 16:10:13 +0500

    0.0.1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(20)->create();
-
+//        \App\Models\User::factory(20)->create();
+//
         \App\Models\User::factory()->create([
             'name' => 'Admin2 Admin2',
             'email' => 'aminjon1065@gmail.com',
